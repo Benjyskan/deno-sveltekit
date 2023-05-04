1f61a7f91341a6a9447ab649a4e989900b9c43a1
Enzo Piriou <enzopiriou@gmail.com>
2023-05-05 00:05:24 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let count = 0;
+
+	const add = () => {
+		count = count + 1;
+	};
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>count: {count}</p>
+<button on:click={add}>+</button>
